950005a8c61c1a3da8db41afb4fc77fe0d0d3d7c
caues <caues@SCHMITZNOT>
2025-04-03 20:45:41 -0300

    ex 04 ok

---
+++ ListaDeExercicio.Ex04/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ListaDeExercicio.Ex04
+{
+    internal class Program
+    {
+        //4. Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+        static void Main(string[] args)
+        {
+            #region input
+            Console.WriteLine("A temperatura da escala Celsius: ");
+            double Celsius = Convert.ToDouble(Console.ReadLine());
+            #endregion
+
+            #region metodo
+            //double temperatura =  (9/5 * Celsius + 32);
+            double temperatura = ((Celsius * 9) / 5) + 32;
+            #endregion
+
+            #region output
+            Console.WriteLine($"A temperatura da escala Celsius é: {temperatura}");
+            Console.ReadLine();
+            #endregion
+
+
+           
+        }
+    }
+}
