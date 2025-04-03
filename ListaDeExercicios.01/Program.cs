b24a70ddf0b650ac4075fa669c50056bf63bb2d7
caues <caues@SCHMITZNOT>
2025-04-03 19:58:21 -0300

    Add project files.

---
+++ ListaDeExercicios.01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ListaDeExercicios._01
+{
+    internal class Program
+    {
+
+        //1. Crie um programa para calcular o volume de uma caixa retangular
+        // volume da caixa comp + largura + altura
+        static void Main(string[] args)
+        {
+            #region input de dados
+            Console.WriteLine("digite o comprimento da ciaxa");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("digite a largura da ciaxa");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("digite a altura da ciaxa");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            #region processamento
+            decimal resultado = comprimento + largura + altura;
+            #endregion
+
+            #region output
+            Console.WriteLine($"O volume da caixa é: {resultado}.");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ listaDeExercicios.Ex02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace listaDeExercicios.Ex02
+{
+    internal class Program
+    {
+        //2. Crie um programa para calcular o volume de um Cilindro
+        // V = PI .r2 . a
+        static void Main(string[] args)
+        {
+            #region input
+            Console.WriteLine("Digite o raio do cilindro");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do cilindro: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            #endregion
+
+            #region metodo
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+            #endregion
+
+            #region output
+            Console.WriteLine($" O raio do cilindro é: {raio}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
