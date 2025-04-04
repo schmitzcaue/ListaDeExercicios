216359ff9e6b2acad1d889a9f56dd98906e4d765
caues <caues@SCHMITZNOT>
2025-04-03 21:43:45 -0300

    ex 07

---
+++ ListaDeExercicios.07/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ListaDeExercicios.Exercicio07
+{
+    internal class Program
+    {
+        //Crie um programa para verificar se um número é primo.
+        static void Main(string[] args)
+        {
+            #region imput de dados
+            Console.WriteLine("Digite o numero");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            #endregion
+
+            #region processamento
+            bool ehPrimo = true;
+
+            // Se o número for menor ou igual a 1, ele não é primo
+            if (numero <= 1)
+            {
+                ehPrimo = false;
+            }
+            else
+            {
+                // Verifica se o número tem algum divisor além de 1 e ele mesmo
+                for (int i = 2; i <= Math.Sqrt(numero); i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        ehPrimo = false;
+                        break;
+                    }
+                }
+            }
+            #endregion
+
+            #region output dos dados
+            if (ehPrimo)
+            {
+                Console.WriteLine($"{numero} é um número primo.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} não é um número primo.");
+            }
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ListaDeExercicios.Ex08/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ListaDeExercicios.Ex08
+{
+    internal class Program
+    {
+       // 8. Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+        static void Main(string[] args)
+        {
+            #region input
+            Console.WriteLine("Digite o valor A: ");
+            double valorA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor B: ");
+            double valorB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor C: ");
+            double valorC = Convert.ToDouble(Console.ReadLine());
+
+
+            #endregion
+
+            #region metodo
+            double resultado =
+
+            #endregion
+
+            #region output
+            Console.WriteLine($"A media do aluno é: {resultado}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
